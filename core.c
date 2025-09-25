7150fdb85536e45a4ec43fea3087469b3d36a7b9
sunwook <sw98@hwajintni.co.kr>
2025-09-26 08:49:53 +0900

    feat(core.c): 첫 타이핑 시작
    - 작업용 주석 추가
    - 소프트웨어 라이센스 주석 일부 작성 중

---
+++ core.c
@@ -0,0 +1,11 @@
+﻿// SPDX-License-Identifier: GPL-2.0
+/**
+ * Performance events core code:
+ *
+ *  Copyright (C) 2008 Thomas Gleixner <tglx@linutronix.de>
+ * @remarks
+ * 여기서부터 작업 예정
+ *
+ *  Copyright (C) 2008-2011 Red Hat,
+ *
+ */
