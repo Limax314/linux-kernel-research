a83d29f412be4644b03e7901e31f6bba2523703a
sunwook <sw98@hwajintni.co.kr>
2025-09-29 12:54:33 +0900

    refactor(core.c): 저작권 주석 추가
    - 작성자명 일부 추가 중

--- core.c
+++ core.c
@@ -3,9 +3,8 @@
  * Performance events core code:
  *
  *  Copyright (C) 2008 Thomas Gleixner <tglx@linutronix.de>
+ *  Copyright (C) 2008-2011 Red Hat, Inc., Ingo Molnar
+ *  Copyright (C) 2008-2011 Red Hat, Inc., Peter Zijlstra
  * @remarks
  * 여기서부터 작업 예정
- *
- *  Copyright (C) 2008-2011 Red Hat,
- *
  */
