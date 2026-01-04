b82d8fb1808b7128a52f9aee8faf7c6dd6f24313
Limax314 <thong31415@gmail.com>
2026-01-04 23:06:16 +0900

    feat(core.c): remote_function 완성 - 로직 및 문법 파악 예정

--- core.c
+++ core.c
@@ -78,13 +78,21 @@ remote_function(void *data)
     if (task_cpu(p) != smp_processor_id())
       return;
 
-    /**
-     * @remarks
-     * 여기서부터 작성 예정
+    /*
+     * Now that we're on right CPU with IRQs disabled, we can test
+     * if we hit the right task without races.
      */
 
-    /*
-     *
-     */
+    tfc->ret = -ESRCH; /* No such (running) process */
+
+    if (p != current)
+      return;
   }
+
+  tfc->ret = tfc->func(tfc->info);
 }
+
+/**
+ * @remarks
+ * 여기서부터 작성 예정
+ */
